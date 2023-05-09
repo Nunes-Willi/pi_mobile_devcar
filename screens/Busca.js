3529ca59a680275a11481dbafac7ccf84aea98fb
Nunes-Will <nuneswillroberto@gmail.com>
2023-05-09 15:56:58 -0300

    Add img em busca

--- screens/Busca.js
+++ screens/Busca.js
@@ -1,33 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+  Image,
+} from "react-native";
 
 const cars = [
   {
-    id: '1',
-    name: 'Honda Civic',
-    description: 'A reliable and efficient car.',
+    id: "1",
+    name: "Honda Civic",
+    description: "A reliable and efficient car.",
+    image:
+      "https://quatrorodas.abril.com.br/wp-content/uploads/2022/04/honda-civic-seda-e1650568274124.jpg?quality=70&strip=info&w=1280&h=720&crop=1",
   },
   {
-    id: '2',
-    name: 'Toyota Corolla',
-    description: 'A popular car with good resale value.',
+    id: "2",
+    name: "Toyota Corolla",
+    description: "A popular car with good resale value.",
+    image:
+      "https://media.toyota.com.ar/7fa6ab27-3617-4a5f-9e38-d092b862411e.jpeg",
   },
   {
-    id: '3',
-    name: 'Mazda 3',
-    description: 'A sporty and fun-to-drive car.',
+    id: "3",
+    name: "Mazda 3",
+    description: "A sporty and fun-to-drive car.",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a7/Mazda3_SKYACTIV-G_%28cropped%29.jpg",
   },
 ];
 
 export default function Search() {
-  const [query, setQuery] = React.useState('');
+  const [query, setQuery] = React.useState("");
   const [results, setResults] = React.useState([]);
+  const [images, setImages] = React.useState([]);
 
   const handleSearch = () => {
     const filteredResults = cars.filter((car) =>
       car.name.toLowerCase().includes(query.toLowerCase())
     );
     setResults(filteredResults);
+    setImages(filteredResults.map((car) => car.image));
   };
 
   return (
@@ -41,10 +56,13 @@ export default function Search() {
       />
       <FlatList
         data={results}
-        renderItem={({ item }) => (
+        renderItem={({ item, index }) => (
           <View style={styles.result}>
-            <Text style={styles.title}>{item.name}</Text>
-            <Text>{item.description}</Text>
+            <Image source={{ uri: images[index] }} style={styles.image} />
+            <View style={styles.textContainer}>
+              <Text style={styles.title}>{item.name}</Text>
+              <Text>{item.description}</Text>
+            </View>
           </View>
         )}
         keyExtractor={(item) => item.id}
@@ -53,30 +71,68 @@ export default function Search() {
   );
 }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 20,
+//   },
+//   input: {
+//     width: "100%",
+//     height: 40,
+//     borderColor: "#ccc",
+//     borderWidth: 1,
+//     borderRadius: 5,
+//     marginBottom: 20,
+//     paddingHorizontal: 10,
+//   },
+//   result: {
+//     borderColor: "#ccc",
+//     borderWidth: 1,
+//     borderRadius: 5,
+//     padding: 10,
+//     marginBottom: 10,
+//   },
+//   title: {
+//     fontSize: 16,
+//     fontWeight: "bold",
+//     marginBottom: 5,
+//   },
+// });
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
   },
   input: {
-    width: '100%',
+    width: "100%",
     height: 40,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 1,
     borderRadius: 5,
     marginBottom: 20,
     paddingHorizontal: 10,
   },
   result: {
-    borderColor: '#ccc',
+    flexDirection: "row",
+    borderColor: "#ccc",
     borderWidth: 1,
     borderRadius: 5,
     padding: 10,
     marginBottom: 10,
   },
+  image: {
+    width: 80,
+    height: 80,
+    resizeMode: "cover",
+    marginRight: 10,
+  },
+  textContainer: {
+    flex: 1,
+  },
   title: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
   },
 });
